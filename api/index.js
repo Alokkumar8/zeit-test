e0f85c2c76ebf45931da81bcea797da7431b2460
Alok Kumar <Alokkumar8@users.noreply.github.com>
2019-11-09 13:30:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export default (req, res) => {
 
   const response_type = 'in_channel'
   // const message = 'If you haven\'t failed, you haven\'t tried'
-  const message = 'Ik Onkar'
+  const message = 'Hello slack'
 
   res.json({ response_type, text: message })
 }
